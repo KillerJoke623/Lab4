e7ea9d3d82e180803d16ed6ce8b89100cec28c1c
KillerJoke623 <91372475+KillerJoke623@users.noreply.github.com>
2022-06-07 16:51:41 +0300

    CarPartService bugs fixed

--- Lab4/Data/DTOs/CarPartDTO.cs
+++ Lab4/Data/DTOs/CarPartDTO.cs
@@ -2,7 +2,7 @@
 
 public class CarPartDTO
 {
-    public int Id { get; set; }
+    public int Id { get; }
     public string Name { get; set; }
     public int Price { get; set; }
     public string BrandOfAuto { get; set; }
--- Lab4/Data/Services/CarPartService.cs
+++ Lab4/Data/Services/CarPartService.cs
@@ -11,14 +11,9 @@ public class CarPartService
     {
         _context = context;
     }
-    //BUG The type of navigation 'CarPart.Sellers' is '<IntersectByIterator>d__118<Seller, int>' which does not implement 'ICollection<Seller>'. Collection navigations must implement 'ICollection<>' of the target type.
+    
     public async Task<CarPart> AddCarPart(CarPartDTO carPart)
     {
-        //Lab4
-        /*
-        DataSource.GetInstance()._carParts.Add(carPart);
-        return await Task.FromResult(carPart);
-        */
         CarPart nCarPart = new CarPart()
         {
             Name = carPart.Name,
@@ -28,7 +23,7 @@ public class CarPartService
         };
         if (carPart.Sellers.Any())
         {
-            nCarPart.Sellers  = _context.Sellers.ToList().IntersectBy(carPart.Sellers, sel => sel.ID);
+            nCarPart.Sellers  = _context.Sellers.ToList().IntersectBy(carPart.Sellers, sel => sel.ID).ToList();
         }
         
         var result = _context.CarParts.Add(nCarPart);
@@ -47,13 +42,12 @@ public class CarPartService
         var result = await _context.CarParts.Include(p => p.Sellers).ToListAsync();
         return await Task.FromResult(result);
     }
-    //BUG Get rid of Id input Rewrite to Lab5
-    //System.InvalidOperationException: The expression '(se.Id == __newCarPart_Id_0)' is invalid inside an 'Include' operation, since it does not represent a property access: 't => t.MyProperty'. To target navigations declared on derived types, use casting ('t => ((Derived)t).MyProperty') or the 'as' operator ('t => (t as Derived).MyProperty'). Collection navigation access can be filtered by composing Where, OrderBy(Descending), ThenBy(Descending), Skip or Take operations. For more information on including related data, see http://go.microsoft.com/fwlink/?LinkID=746393.
-    //at Microsoft.EntityFrameworkCore.Query.Internal.NavigationExpandingExpressionVisitor.<ProcessInclude>g__ExtractIncludeFilter|32_0(Expression currentExpression, Expression includeExpression)
+    //BUG don't delete Sellers of CarPart
     public async Task<CarPart?> UpdateCarPart(int id, CarPart newCarPart)
     {
         
-        var carPart = await _context.CarParts.Include(se => se.Id == newCarPart.Id).FirstOrDefaultAsync(cp => cp.Id==id);
+        var carPart = await _context.CarParts.Include(cp=>cp.Sellers).FirstOrDefaultAsync(cp => cp.Id == id); 
+        
 
         if (carPart != null)
         {
@@ -73,13 +67,14 @@ public class CarPartService
         }
         return null;
     }
-
+    
     public async Task<bool> DeleteCarPart(int id)
     {
-        var carPart = await _context.CarParts.FirstOrDefaultAsync(s => s.Id == id);
+        var carPart = await _context.CarParts.FirstOrDefaultAsync(cp => cp.Id == id);
         if (carPart != null)
         {
-            DataSource.GetInstance()._carParts.Remove(carPart);
+            _context.CarParts.Remove(carPart);
+            await _context.SaveChangesAsync();
             return true;
         }
 
